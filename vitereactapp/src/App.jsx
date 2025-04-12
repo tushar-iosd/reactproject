668168ae80952ddfe5708da107b322a6cb6219a4
Tushar Sharma <tushar.sharma3@soprasteria.com>
2025-04-12 20:28:57 +0530

    Exported multiple components from App.jsx

--- vitereactapp/src/App.jsx
+++ vitereactapp/src/App.jsx
@@ -4,8 +4,14 @@ import Chai from './chai.jsx';
 function App() {
 
 return (
-  <Chai/>
-);
+//  <Chai/>
+
+  <div>
+    <h1>Vite + React + Chai</h1>
+    <Chai />
+    <h2>Vite + React + Chai</h2>
+  </div>
+  )
 }
 
 export default App
